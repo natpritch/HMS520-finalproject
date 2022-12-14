49713a3a7e2f35208c651057a3d79d10cf351499
natpritch <94652229+natpritch@users.noreply.github.com>
2022-12-14 10:50:34 -0800

    Add code to make dataset
    Adding simulated data to the package to use for output examples

--- R code/GBD Vetting Plots.R
+++ R code/GBD Vetting Plots.R
@@ -33,8 +33,40 @@ theme_set(theme_bw(16))
 #Make dataset
 ###################################
 
-dat <- read.csv("dataset/gbd vetting plots.csv")
-dat$sex <- 2
+#create data frame to house character variable cause
+cause <- rep("cervical cancer", times=50)
+
+#create random binary data type variable and assign values to 0 and 1
+data_type_random <- rbinom(50, 1, .5)
+data_type <- ifelse(data_type_random == 0, "VR", "CR")
+
+#create garbage code variable that is between 1 and 100 percent
+g_code <- runif(50, 1, 100)
+
+#create country id and countrie varianle and assign values to 0 and 1
+country_id <- rbinom(50, 1, .5)
+countries <- ifelse(country_id == 0, "Colombia", "Costa Rica")
+
+#create year id with repeating values
+year_id <- sample(1990:2015, 50, replace=TRUE)
+
+#create age group id with repeating values
+age_group_id <- sample(5:21, 50, replace = TRUE)
+
+# create female only id repeating 50 times
+sex_id <-rep(2,times=50)
+
+# combine them into one data fram 
+practice <- cbind.data.frame(data_type, g_code, country_id, countries, year_id, age_group_id, sex_id, cause)
+
+#reassign country id to refelct mock gbd location id
+practice$country_id[practice$country_id == 0] <- 28
+practice$country_id[practice$country_id == 1] <- 27
+
+#export mock data as csv for data manipulation
+write_csv(practice, "Final Project.csv")
+
+dat <- practice
 
 ##################################
 #Options for user to select
